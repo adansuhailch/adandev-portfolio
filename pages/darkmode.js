1ea5432c37c9f3f9266a82142d2f28184e35474a
adansuhailch-cell <adansuhailch@gmail.com>
2026-06-14 19:03:23 -0700

    Update Portfolio and add a loading screen before starting iy.

--- pages/darkmode.js
+++ pages/darkmode.js
@@ -12,13 +12,52 @@ const disableDarkmode = () => {
     localStorage.setItem('darkmode', 'inactive')
 }
 
-if(darkmode === 'active'){
+if (darkmode === 'active') {
     eneableDarkmode()
 }
 
 
-themeSwitch.addEventListener("click", () =>{
+themeSwitch.addEventListener("click", () => {
     darkmode = localStorage.getItem('darkmode')
 
     darkmode !== 'active' ? eneableDarkmode() : disableDarkmode()
 })
+
+// loading animation code start before the page is loaded
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loader");
+
+    const progressBar =
+        document.querySelector(".progress-bar");
+
+    const percentage =
+        document.querySelector(".percentage");
+
+    let progress = 0;
+
+    const interval = setInterval(() => {
+
+        progress += 5;
+
+        progressBar.style.width =
+            `${progress}%`;
+
+        percentage.textContent =
+            `${progress}%`;
+
+        if (progress >= 100) {
+
+            clearInterval(interval);
+
+            setTimeout(() => {
+
+                loader.classList.add("hide");
+
+            }, 300);
+
+        }
+
+    }, 80);
+
+});
